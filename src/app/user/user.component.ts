74cafe0b25fe0de6a71f2591fee850a46959a280
osoji <osoji@simplexsystem.com>
2024-06-24 16:37:14 +0100

    Manage State and changing data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,5 +12,20 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath(){
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+
+  get userName(){
+    return this.selectedUser.name
+  }
+
+  //How to listening to event e.g click event and state management
+  onSelectUser(){
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+
 }
