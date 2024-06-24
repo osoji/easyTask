4dfebcb9cefa827dcb11e9bf2f8f9c13c2ea7c96
osoji <osoji@simplexsystem.com>
2024-06-24 19:38:20 +0100

    Using signal and decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +7,35 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {}
+//NOTE: the capita letter Input is a decorator while the small letter is special function
+
+//Since signal is new to angular I will go with decorator
+
+
+//signal method
+  //avatar = input('');
+  //avatar = input<string>('');
+  
+  //Or better put
+  //avatar = input.required<string>(); // this is better than decorator
+  //name = input.required<string>();
+
+  //using singnal for imagePath
+  // imagePath = computed(()=>{
+  //   return 'assets/users/' + this.avatar;
+  // });
+
+
+  onSelectUser() {
+
+
+
+  }
 }
