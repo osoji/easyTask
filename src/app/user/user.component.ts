3fcbe157fb40b48c6e53bdf1845faf80b2c3be68
osoji <osoji@simplexsystem.com>
2024-06-24 15:33:37 +0100

    computed value

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+//Give me random number between zero(0) and the heighest available number in DUMMY_USERS
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,5 +12,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex]
 }
