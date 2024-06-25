c1d1930284dba56b4077c508b7ac7cc49d72b560
osoji <osoji@simplexsystem.com>
2024-06-25 11:09:32 +0100

    Output And Emitting Data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, computed, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,22 +7,33 @@ import { Component, Input, computed, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  // Decorator method
+  // In Angular, an input property is a way to pass data from a parent
+  // component to a child component. It's a key feature of Angular's component architecture.
+
+  //The function of an input property is to:
+
+  // 1. Allow data to be passed from a parent component to a child component.
+  // 2. Enable the child component to receive and display the data.
+  // 3. Create a one-way data binding between the parent and child components.
+
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
-     return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.avatar;
   }
 
-//NOTE: the capita letter Input is a decorator while the small letter is special function
+  //NOTE: the capita letter Input is a decorator while the small letter is special function
 
-//Since signal is new to angular I will go with decorator
+  //Since signal is new to angular I will go with decorator
 
-
-//signal method
+  //signal method
   //avatar = input('');
   //avatar = input<string>('');
-  
+
   //Or better put
   //avatar = input.required<string>(); // this is better than decorator
   //name = input.required<string>();
@@ -32,10 +43,7 @@ export class UserComponent {
   //   return 'assets/users/' + this.avatar;
   // });
 
-
   onSelectUser() {
-
-
-
+    this.select.emit(this.id);
   }
 }
