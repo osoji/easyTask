ca81cfec437fd6391683e7c43eeafb12a879924d
osoji <osoji@simplexsystem.com>
2024-06-25 14:22:15 +0100

    Using object and interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,18 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, input, output } from '@angular/core';
+
+// type User ={
+//   id: string,
+//   avatar: string,
+//   name: string
+// };
+
+//Or
+
+interface User{
+  id: string,
+  avatar: string,
+  name: string
+}
 
 @Component({
   selector: 'app-user',
@@ -17,13 +31,34 @@ export class UserComponent {
   // 2. Enable the child component to receive and display the data.
   // 3. Create a one-way data binding between the parent and child components.
 
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  //Decorator method for Input
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  //Best way to the above code
+  // @Input({required:true}) user!:{
+  //   id: string,
+  //   avatar: string,
+  //   name: string
+  // };
+
+  //Better way
+  @Input({required:true}) user!:User;
+
+  //function methed for input
+  // id = input.required<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  //Decorator method for Output
+  @Output() select = new EventEmitter(); //Decorator
+
+  //function method for output
+  //select = output<string>(); //function
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   //NOTE: the capita letter Input is a decorator while the small letter is special function
@@ -44,6 +79,6 @@ export class UserComponent {
   // });
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
